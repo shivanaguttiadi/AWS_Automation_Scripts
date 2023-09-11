0be2771d57b058eeb15e79ca5fdb0fd139924c10
Adi Shiva <adi.shiva@zapcg.com>
2023-09-11 19:58:56 +0530

    checked

--- EC2Services_Scripts/EBS_Scripts/ebsdelete.py
+++ EC2Services_Scripts/EBS_Scripts/ebsdelete.py
@@ -5,31 +5,33 @@ from tabulate import tabulate
 # Initialize the EC2 client
 ec2 = boto3.client('ec2')
 
-# Replace 'your-volume-id' with the actual EBS volume ID
-volume_id = 'vol-0249447083d3e1605'
+# Replace 'your-volume-id' with the actual EBS volume IDs
+volume_ids = ['vol-09605cf056d647665', 'vol-0690f4ed13c534a26']
 
 # Record the volume's information before deletion
 with open('deleted_volumes.csv', mode='a', newline='') as file:
     writer = csv.writer(file)
     
-    # Get information about the volume (size and type)
+    # Get information about each volume (size and type)
+    for volume_id in volume_ids:
+        try:
+            describe_response = ec2.describe_volumes(VolumeIds=[volume_id])
+            volume_info = describe_response['Volumes'][0]
+            size = volume_info['Size']
+            volume_type = volume_info['VolumeType']
+            
+            # Write the information to the CSV file
+            writer.writerow([volume_id, size, volume_type])
+        except ec2.exceptions.ClientError as e:
+            print(f"Error: {e}")
+
+# Delete the EBS volumes
+for volume_id in volume_ids:
     try:
-        describe_response = ec2.describe_volumes(VolumeIds=[volume_id])
-        volume_info = describe_response['Volumes'][0]
-        size = volume_info['Size']
-        volume_type = volume_info['VolumeType']
-        
-        # Write the information to the CSV file
-        writer.writerow([volume_id, size, volume_type])
+        ec2.delete_volume(VolumeId=volume_id)
+        print(f"EBS volume {volume_id} deletion requested.")
     except ec2.exceptions.ClientError as e:
         print(f"Error: {e}")
-
-# Delete the EBS volume
-try:
-    ec2.delete_volume(VolumeId=volume_id)
-    print(f"EBS volume {volume_id} deletion requested.")
-except ec2.exceptions.ClientError as e:
-    print(f"Error: {e}")
 
 # Optionally, you can list all previously deleted volumes from the CSV file
 with open('deleted_volumes.csv', mode='r') as file:
--- EC2Services_Scripts/EBS_Scripts/ebsunused.py
+++ EC2Services_Scripts/EBS_Scripts/ebsunused.py
@@ -16,15 +16,23 @@ for volume in response['Volumes']:
     volume_type = volume['VolumeType']
     volume_status = volume['State']
 
+    # Get the volume name from the tags
+    volume_name = ""
+    for tag in volume.get('Tags', []):
+        if tag['Key'] == 'Name':
+            volume_name = tag['Value']
+            break
+
     volume_info.append([
         volume_id,
+        volume_name,  # Include the volume name
         volume_size,
         volume_type,
         volume_status,
     ])
 
 # Create a table and print it
-table_headers = ["Volume ID", "Size (GiB)", "Volume Type", "Volume Status"]
+table_headers = ["Volume ID", "Volume Name", "Size (GiB)", "Volume Type", "Volume Status"]
 table = tabulate(volume_info, headers=table_headers, tablefmt="grid")
 print("List of Available EBS Volumes:")
 print(table)
